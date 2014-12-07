50365504de672898b1f3aba690cfb5c87727a8e1
CaptainMustang8 <nmorris@smu.edu>
2014-12-06 20:33:14 -0600

    InitObject changes
    Needs work on the constructor method, but all else is ready, mostly.
    Also needs input from initial GUI (Title.java)

--- InitObject.java
+++ InitObject.java
@@ -17,39 +17,66 @@ public class InitObject {
     private boolean isServer;
     
     
-    public InitObject(){
-        
-    
+    public InitObject(int newPort, String newMapPath, int newIPAddress, boolean newIsServer){
+        // need to fix this method
+        // requires user input from Title.java
+        // if isServer == true, call the InitServer method
+        // otherwise, call the InitClient method
+        if(isServer == true)
+            InitServer(newPort, newMapPath);
+        else
+            InitClient(newIPAddress);
     }
     
+    
+    // constructor for Server
+    public void InitServer(int newPort, String newMapPath){
+        port = newPort;
+        mapPath = newMapPath;
+    }
+    
+    // constructor for Client
+    public void InitClient(int newIPAddress){
+        ipAddress = newIPAddress;
+    }
+    
+    // Hardcoded, for convenience, to the localhost
+    // Used by Server
     public void setPort(){
         port = 2112;
     }
     
+    // getter for the port
     public int getPort(){
         return port;
     }
     
+    // Useful only for the Server
     public void setMapPath(String path){
         mapPath = path;
     }
     
+    // getter for the mapPath
     public String getMapPath(){
         return mapPath;
     }
     
+    // Useful only for the Client
     public void setIPAddress(int address){
         ipAddress = address;
     }
     
+    // getter for the ipAddress
     public int getIPAddress(){
         return ipAddress;
     }
     
+    // Used to set flag whether object is server or not
     public void setIsServer(boolean server){
         isServer = server;
     }
     
+    // getter for the isServer boolean
     public boolean getIsServer(){
         return isServer;
     }
